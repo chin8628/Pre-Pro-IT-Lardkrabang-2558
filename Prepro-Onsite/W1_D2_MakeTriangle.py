2e421c22d81bfe9d1d814414175e66950926056e
Pichai Sivawat <tingtong003tomy@gmail.com>
2015-06-24 16:19:09 +0700

    no message

--- Prepro-Onsite/W1_D2_MakeTriangle.py
+++ Prepro-Onsite/W1_D2_MakeTriangle.py
@@ -1,12 +1,7 @@
 """ W1_D2_MakeTriangle """
-def main():
-    """ Notice me senpai """
-    input1 = int(input())
-    input2 = int(input())
-    input3 = int(input())
 
-    if input1 > input2+input3 or input2 > input1+input3 or input3 > input2+input1:
-        print("NO")
-    else:
-        print("YES")
-main()
+
+def main(aaa):
+    """ Notice me senpai """
+    print(("NO", "YES")[max(aaa) <= sum(sorted(aaa)[:-1])])
+main([int(input()), int(input()), int(input())])
