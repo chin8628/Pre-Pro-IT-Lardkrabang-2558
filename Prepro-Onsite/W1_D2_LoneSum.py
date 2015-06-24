6173ed6e3e308c970faaf5d3b265c9f80b5cd1d2
Pichai Sivawat <tingtong003tomy@gmail.com>
2015-06-24 16:07:05 +0700

    ninja

--- Prepro-Onsite/W1_D2_LoneSum.py
+++ Prepro-Onsite/W1_D2_LoneSum.py
@@ -1,16 +1,8 @@
 """ W1_D2_LoneSum """
-def main():
-    """ def top """
-    aaa = int(input())
-    bbb = int(input())
-    ccc = int(input())
-    answer = 0
-    if aaa != bbb and aaa != ccc:
-        answer += aaa
-    if bbb != aaa and bbb != ccc:
-        answer += bbb
-    if ccc != aaa and ccc != bbb:
-        answer += ccc
-    print(answer)
 
-main()
+
+def main(aaa):
+    """ def top """
+    print(sum([i for i in aaa if aaa.count(i) < 2]))
+
+main([int(input()), int(input()), int(input())])
