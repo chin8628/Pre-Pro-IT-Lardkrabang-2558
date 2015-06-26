30b9e755eecd9ad6a5a75420f290c1fd841812ff
chin8628 <boon.8628@hotmail.com>
2015-06-26 14:50:03 +0700

    Quiz_W1_Kangaroo

--- Quiz/Quiz_W1_Kangaroo.py
+++ Quiz/Quiz_W1_Kangaroo.py
@@ -1,10 +1,5 @@
 """ Quiz_W1_Kangaroo """
-def main():
+def main(inpa):
     """ So lazy """
-    inputa = [int(input()), int(input()), int(input())]
-    inputa.sort()
-    if inputa[2] - inputa[1] > inputa[1] - inputa[0]:
-        print(inputa[2] - inputa[1] - 1)
-    else:
-        print(inputa[1] - inputa[0] - 1)
-main()
+    print([inpa[1] - inpa[0] - 1, inpa[2] - inpa[1] - 1][inpa[2] - inpa[1] > inpa[1] - inpa[0]])
+main(sorted(map(int, [input(), input(), input()])))
