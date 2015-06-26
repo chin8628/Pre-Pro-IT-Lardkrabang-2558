894a461ff5a1571f693a908480d7c6893f75f138
chin8628 <boon.8628@hotmail.com>
2015-06-26 13:46:20 +0700

    Modified Quiz Kangaroo and Operation

--- Quiz/Quiz_W1_Kangaroo.py
+++ Quiz/Quiz_W1_Kangaroo.py
@@ -2,6 +2,7 @@
 def main():
     """ So lazy """
     inputa = [int(input()), int(input()), int(input())]
+    inputa.sort()
     if inputa[2] - inputa[1] > inputa[1] - inputa[0]:
         print(inputa[2] - inputa[1] - 1)
     else:
--- Quiz/Quiz_W1_Operation.py
+++ Quiz/Quiz_W1_Operation.py
@@ -2,9 +2,8 @@
 import math
 def main():
     """ WOW This is a QUIZ. S0 aWes0me """
-    pi = math.pi
     wow = (((4 * 4) + (2 * 2))**0.5)**2
-    wow = wow * (pi * pi * pi * pi)
-    wow = wow * math.cos(pi)
+    wow = wow * (math.pi * math.pi * math.pi * math.pi)
+    wow = wow * math.cos(math.pi)
     print(wow)
 main()
