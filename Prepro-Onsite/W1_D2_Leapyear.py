f0aa625ab45fcf20bce5a65aae26d030d63ecea0
PhompAng <tingtong003tomy@gmail.com>
2015-06-27 22:19:35 +0700

    more ninja

--- Prepro-Onsite/W1_D2_Leapyear.py
+++ Prepro-Onsite/W1_D2_Leapyear.py
@@ -1,2 +1,2 @@
 """ W1_D2_Leapyear """
-any([i for i in filter(lambda x: print(x%4 == 0 and x%100 != 0 or x%400 == 0), [int(input())-543])])
+any(filter(lambda x: print(x%4 == 0 and x%100 != 0 or x%400 == 0), [int(input())-543]))
